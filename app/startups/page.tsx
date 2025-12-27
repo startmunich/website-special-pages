0b0b9866f8716eb252b9877e7be93e5505dbd66a
Simon Burmer <simon.burmer@sap.com>
2025-12-27 22:09:09 +0100

    test

--- app/startups/page.tsx
+++ app/startups/page.tsx
@@ -245,7 +245,7 @@ export default function StartupsPage() {
 
       <main className="min-h-screen bg-[#00002c]">
         {/* Hero Section with Full-Width Image */}
-        <div className="relative w-full overflow-hidden h-[30vh]">
+        <div className="relative w-full overflow-hidden h-[600px]">
           {/* Background Image + Overlay */}
           <div className="absolute inset-0 h-full">
             <img
